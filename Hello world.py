5e4a96892d51735e97e804e2c7af06d50ae70541
leslly <leslican101@gmail.com>
2022-10-11 14:03:37 +0100

    first commit

--- Hello world.py
+++ Hello world.py
@@ -1,2 +1,2 @@
-// my first python code
+
 print("hello guy's")
---
+++ main.py
@@ -0,0 +1,10 @@
+print("------------------Calculator------------------")
+number1 = input("Input Num1: ")
+number2 = input("Input Num2: ")
+
+print("1. +  2. -  3. x  4. /")
+
+operation = input("enter an operation: ")
+
+if(operation == '+'):
+    print(number1 + number2)
